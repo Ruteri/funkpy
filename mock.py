8d4ca85ef19f7af8651255306074a628a5ecee02
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2020-01-16 13:08:58 +0100

    Mock: code reorganization and minor refactor
    Signed-off-by: Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>

--- mock.py
+++ mock.py
@@ -1,5 +1,61 @@
 import re
 
+# Various helpers
+def _raise(ex):
+    raise ex
+
+# Argument matching
+def _normalize_args(arg_map, *args, **kwargs):
+    """Normalizes arguments against arguments map
+
+    Returns:
+        * tuple of positional arguments if arguments map is None
+        * dict of keyword arguments using arguments map to translate positionals
+    """
+    if arg_map is None and kwargs:
+        raise AssertionError('cannot merge kwargs without function definition')
+
+    elif arg_map is None:
+        return args
+
+    all_args_kw = kwargs
+    for index, arg in enumerate(args):
+        varname = arg_map[index]
+        all_args_kw[varname] = arg
+
+    return all_args_kw
+
+
+def _args_match(expected, actual):
+    """Checks whether merged arguments match.
+
+    Uses equality to match arguments.
+
+    Arguments: lists of positional arguments or dicts of keyword arguments
+    """
+    if type(expected) != type(actual):
+        # Mismatch due to lack of function definition
+        return False
+
+    if isinstance(expected, dict):
+        if sorted(expected.keys()) != sorted(actual.keys()): return False
+        for k, v in expected.items():
+            av = actual[k]
+            if v == av: continue
+            return False
+        return True
+
+    if isinstance(expected, tuple):
+        if len(expected) != len(actual): return False
+        for i, v in enumerate(expected):
+            av = actual[i]
+            if v == av: continue
+            return False
+        return True
+
+    return False
+
+
 class OfType(object):
     """Type argument matcher."""
     def __init__(self, expected):
@@ -12,12 +68,55 @@ class OfType(object):
 class Like(object):
     """Regex argument matcher."""
     def __init__(self, expected):
+        self.pattern = expected
         self.expected = re.compile(expected)
 
     def __eq__(self, other):
         return self.expected.match(other) is not None
 
+    def __repr__(self):
+        return '<Like object, pattern="{}">'.format(self.pattern)
 
+    def __str__(self):
+        return 'Like object, pattern="{}"'.format(self.pattern)
+
+
+# Expectation Checks
+class ExpectationCheck(object):
+    def __call__(self, expectation):
+        raise RuntimeError('abstract expectation called')
+
+
+class CalledExactly(ExpectationCheck):
+    def __init__(self, n_times):
+        self.n_times = n_times
+
+    def __call__(self, expectation):
+        if len(expectation.calls) != self.n_times:
+            raise AssertionError('{} was called {} time(s) but expected exactly {} time(s)'.format(expectation.method_name, len(expectation.calls), self.n_times))
+
+
+class CalledAtLeast(ExpectationCheck):
+    def __init__(self, n_times):
+        self.n_times = n_times
+
+    def __call__(self, expectation):
+        if len(expectation.calls) < self.n_times:
+            raise AssertionError('{} was called {} times but expected at least{}'.format(expectation.method_name, len(expectation.calls), self.n_times))
+
+
+class CalledWith(ExpectationCheck):
+    def __init__(self, args):
+        self.args = args
+
+    def __call__(self, expectation):
+        for call in expectation.calls:
+            if _args_match(self.args, call):
+                return True
+        raise AssertionError('{} was not called with {}'.format(expectation.method_name, self.args))
+
+
+# User-facing classes (Expectation and Mock)
 class Expectation(object):
     """Class Expectation. Holds logic and data on how given function should behave.
 
@@ -41,95 +140,44 @@ class Expectation(object):
 
     def __init__(self, fn=None, method_name=''):
         self.fn = fn
+        self.method_name = method_name
+
         self.arg_map = None
         if self.fn is not None:
             self.arg_map = list(self.fn.__code__.co_varnames)
 
-        self.expects_once = False
-        self.expects_never = False
-        self.n_calls = 0
-        self.to_raise = None
-        self.rv = None
-        self.method_name = method_name
+        self._returns = lambda: None
+
+        self.calls = []
+        self.checks = []
         self.call_expectations = []
 
-    def _on_call_checks(self):
-        self.n_calls += 1
-        if self.expects_never:
-            raise AssertionError('{} was called but expected never'.format(self.method_name))
-
-        if self.expects_once and self.n_calls > 1:
-            raise AssertionError('{} was called more than once but expected once'.format(self.method_name))
-
-    def _merge_kwargs(self, *args, **kwargs):
-        if self.fn is None or self.arg_map is None:
-            if kwargs:
-                raise AssertionError('cannot merge kwargs without function definition')
-            return args
-
-        all_args_kw = kwargs
-        for index, arg in enumerate(args):
-            varname = self.arg_map[index]
-            all_args_kw[varname] = arg
-        return all_args_kw
-
-    def _args_match(self, expected, actual):
-        # Strict matches (None != absent)
-        if expected == actual:
-            return True
-
-        if type(expected) != type(actual):
-            # Mismatch due to lack of function definition
-            return False
-
-        if isinstance(expected, dict):
-            if sorted(expected.keys()) != sorted(actual.keys()): return False
-            for k, v in expected.items():
-                av = actual[k]
-                if v == av: continue
-                return False
-            return True
-
-        if isinstance(expected, list):
-            if len(expected) != len(actual): return False
-            for i, v in enumerate(expected):
-                av = actual[i]
-                if v == av: continue
-                return False
-            return True
-        return False
-
-    def _on_call_dispatch(self, *args, **kwargs):
-        merged_args = self._merge_kwargs(*args, **kwargs)
-        for expected_call in self.call_expectations:
-            if self._args_match(merged_args, expected_call[0]):
-                return expected_call[-1](*args, **kwargs)
-        
-        if self.to_raise is not None:
-            raise self.to_raise
-
-        return self.rv
-
     def __call__(self, *args, **kwargs):
-        self._on_call_checks()
-        return self._on_call_dispatch(*args, **kwargs)
+        merged_args = _normalize_args(self.arg_map, *args, **kwargs)
+        self.calls.append(merged_args)
+        for expected_call in self.call_expectations:
+            if _args_match(merged_args, expected_call[0]):
+                return expected_call[-1](*args, **kwargs)
+
+        return self._returns()
 
     def restore(self):
         """Reset all expectations."""
-        self.__init__(self, self.fn, self.method_name)
+        self.__init__(self.method_name, self.fn)
+        return self
 
     def on(self, *args, **kwargs):
         """Specify action when arguments match.
 
         Only possible to specify for a known function signature (passed in constructor).
         Matches arguments in order of calls (first .on will have the highest priority).
-        
+
         Arguments:
             positional and keyword arguments that rule should match
             equality check will be used to match the argument (__eq__)
 
         Usage:
-            mf.on(5) # will match 
+            mf.on(5) # will match
             mf.on(somekwarg='string')
             mf.on(7, mixedargs=10)
             mf.on(somearg=Like('a[0-9]'))
@@ -137,23 +185,31 @@ class Expectation(object):
 
         Returns expectation.
         """
-        call_matcher = self._merge_kwargs(*args, **kwargs)
-        self.call_expectations.append((call_matcher, Expectation(fn=self.fn, method_name=self.method_name)))
+        call_matcher = _normalize_args(self.arg_map, *args, **kwargs)
+        self.call_expectations.append((call_matcher, Expectation(method_name=self.method_name, fn=self.fn)))
         return self.call_expectations[-1][-1]
-
-    def once(self):
-        """Throws assertion error if called more than once."""
-        self.expects_once = True
-        return self
 
     def never(self):
         """Verification fails with assertion error if never called."""
-        self.expects_never = True
+        self.checks.append(CalledExactly(0))
         return self
+
+    def once(self):
+        """Throws assertion error if called more than once."""
+        self.checks.append(CalledExactly(1))
+        return self
+
+    def at_least(self, n_times):
+        self.checks.append(CalledAtLeast(n_times))
+        return self
+
+    def called_with(self, *args, **kwargs):
+        merged_args = _normalize_args(self.arg_map, *args, **kwargs)
+        self.checks.append(CalledWith(merged_args))
 
     def returns(self, rv):
         """Makes given expectation return rv."""
-        self.rv = rv
+        self._returns = lambda: rv
         return self
 
     def raises(self, ex):
@@ -165,23 +221,16 @@ class Expectation(object):
         Usage:
             mf.raises(ValueError(5))
         """
-        self.to_raise = ex
+        self._returns = lambda: _raise(ex)
         return self
 
     def verify(self):
-        """Verifies expectation.
-
-        Checks whether expectation was called correct number of times.
-        """
-        if self.expects_never and self.n_calls > 0:
-            raise AssertionError('method {} was called but expected never'.format(self.method_name))
-        if self.expects_once and self.n_calls < 1:
-            raise AssertionError('method {} not called but expected once'.format(self.method_name))
-        if self.expects_once and self.n_calls > 1:
-            raise AssertionError('method {} called {} times but expected once'.format(self.method_name, self.n_calls))
+        """Verifies expectations."""
+        for check in self.checks:
+            check(self)
         return True
 
-    
+
 class Mock(Expectation):
     """Mock class. Manages Expectations as member functions.
 
@@ -196,8 +245,8 @@ class Mock(Expectation):
         method_name: self-explanatory, used for debug prints
     """
 
-    def __init__(self, fn=None, method_name=''):
-        Expectation.__init__(self, fn=fn, method_name=method_name)
+    def __init__(self, method_name='', fn=None):
+        Expectation.__init__(self, method_name=method_name, fn=fn)
         self.member_mocks = []
 
     def restore(self):
@@ -205,45 +254,48 @@ class Mock(Expectation):
         for method_name in self.member_mocks:
             delattr(self, method_name)
         Expectation.restore(self)
+        return self
 
     def expects(self, method_name='', fn=None):
         """Creates new member mock and returns it."""
         self.member_mocks.append(method_name)
-        setattr(self, method_name, Mock(fn=fn, method_name=method_name))
+        setattr(self, method_name, Mock(method_name=method_name, fn=fn))
         return getattr(self, method_name)
-        
+
     def verify(self):
         """Verifies all member mocks."""
         for mf in self.member_mocks:
             getattr(self, mf).verify()
         return Expectation.verify(self)
 
+
+# Tests
 import unittest
 
 class MockHelpersTests(unittest.TestCase):
     def test_kwargs_merger(self):
         def f(targ1='', targ2=''): pass
-        m = Mock(fn=f)
-        self.assertEqual(m._merge_kwargs(targ1=1, targ2=2), {'targ1': 1, 'targ2': 2})
-        self.assertEqual(m._merge_kwargs(1, targ2=2), {'targ1': 1, 'targ2': 2})
-        self.assertEqual(m._merge_kwargs(1, 2), {'targ1': 1, 'targ2': 2})
-        self.assertEqual(m._merge_kwargs(targ1=1), {'targ1': 1})
-        self.assertEqual(m._merge_kwargs(targ2=2), {'targ2': 2})
-        self.assertEqual(m._merge_kwargs(1), {'targ1': 1})
+        arg_map = list(f.__code__.co_varnames)
+
+        self.assertEqual(_normalize_args(arg_map, targ1=1, targ2=2), {'targ1': 1, 'targ2': 2})
+        self.assertEqual(_normalize_args(arg_map, 1, targ2=2), {'targ1': 1, 'targ2': 2})
+        self.assertEqual(_normalize_args(arg_map, 1, 2), {'targ1': 1, 'targ2': 2})
+        self.assertEqual(_normalize_args(arg_map, targ1=1), {'targ1': 1})
+        self.assertEqual(_normalize_args(arg_map, targ2=2), {'targ2': 2})
+        self.assertEqual(_normalize_args(arg_map, 1), {'targ1': 1})
 
     def test_args_matcher(self):
         def f(targ1, targ2): pass
-        m = Mock(fn=f)
+        arg_map = list(f.__code__.co_varnames)
+        matcher = {'targ1': 5}
 
-        on_arg1 = m.on(5)
-        matcher = m.call_expectations[-1][0]
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(5)), True)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(targ1=5)), True)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(6)), False)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(targ1=6)), False)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(5, None)), False)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(5, targ2=None)), False)
-        self.assertEqual(m._args_match(matcher, m._merge_kwargs(targ1=5, targ2=None)), False)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, 5)), True)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, targ1=5)), True)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, 6)), False)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, targ1=6)), False)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, 5, None)), False)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, 5, targ2=None)), False)
+        self.assertEqual(_args_match(matcher, _normalize_args(arg_map, targ1=5, targ2=None)), False)
 
 class MockTests(unittest.TestCase):
     def test_no_fn(self):
@@ -330,8 +382,7 @@ class MockTests(unittest.TestCase):
 
         self.assertEqual(m.verify(), True)
 
-        with self.assertRaises(AssertionError):
-            m()
+        m()
 
         with self.assertRaises(AssertionError):
             m.verify()
@@ -343,11 +394,63 @@ class MockTests(unittest.TestCase):
 
         self.assertEqual(m.verify(), True)
 
-        with self.assertRaises(AssertionError):
-            m()
+        m()
 
         with self.assertRaises(AssertionError):
             m.verify()
+
+    def test_at_least(self):
+        m = Mock()
+        m.at_least(2)
+
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m() # 1
+
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m() # 2
+
+        self.assertEqual(m.verify(), True)
+
+        m()
+
+        self.assertEqual(m.verify(), True)
+
+    def test_called_with(self):
+        def f(a, b): pass
+        m = Mock(fn=f)
+        m.called_with(5, b='a')
+
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m(6, 'a')
+        m(5, 'b')
+        m(5, b='b')
+        m(b=5, a='a')
+
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m(5, 'a')
+        self.assertEqual(m.verify(), True)
+
+        m.restore().called_with(5, b='a')
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m(5, b='a')
+        self.assertEqual(m.verify(), True)
+
+        m.restore().called_with(5, b='a')
+        with self.assertRaises(AssertionError):
+            m.verify()
+
+        m(a=5, b='a')
+        self.assertEqual(m.verify(), True)
 
     def test_verify(self):
         m = Mock()
@@ -357,14 +460,13 @@ class MockTests(unittest.TestCase):
         self.assertEqual(m.verify(), True)
         self.assertEqual(mf.verify(), True)
 
-        with self.assertRaises(AssertionError):
-            m.memfn()
-
-        with self.assertRaises(AssertionError):
-            mf.verify()
+        m.memfn()
 
         with self.assertRaises(AssertionError):
             m.verify()
+
+        with self.assertRaises(AssertionError):
+            mf.verify()
 
 class MockBasicUsageTests(unittest.TestCase):
     def test_basic_usage(self):
